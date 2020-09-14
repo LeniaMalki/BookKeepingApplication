febdbc299816c64bfef44e50b925fa6112af19e4
artinabiri <kevinabiri@gmail.com>
2020-09-14 17:09:26 +0200

    lagt till funktionalitet & load in  av header

--- src/main/java/controllers/headerController.java
+++ src/main/java/controllers/headerController.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class headerController {
 
@@ -32,13 +36,4 @@ public class headerController {
     private Hyperlink goalsButton;
 
 
-    public void start(Stage stage) throws Exception{
-
-        Parent root = FXMLLoader.load(getClass().getResource("header.fxml"));
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-    }
 }
