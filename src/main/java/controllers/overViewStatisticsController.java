4c8c5b02bc7351bf79524659f612a932729f7bd3
Oscaarr <43784002+oscarforsbergg@users.noreply.github.com>
2020-09-14 16:40:20 +0200

    /Scene builder, skeleton for controllers, forgot text, Oscar F

--- src/main/java/controllers/overViewStatisticsController.java
+++ src/main/java/controllers/overViewStatisticsController.java
@@ -6,27 +6,38 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class overViewStatisticsController {
 
-    @FXML
-    private Button toDailyStatisticsButton;
+        @FXML
+        private Button toDailyStatisticsButton;
 
-    @FXML
-    private Button toMonthlyStatisticsButton;
+        @FXML
+        private Button toMonthlyStatisticsButton;
 
-    @FXML
-    private Button toWeeklyStatisticsButton;
+        @FXML
+        private Button toWeeklyStatisticsButton;
 
-    @FXML
-    private PieChart dailyOverviewPieChart;
+        @FXML
+        private PieChart dailyOverviewPieChart;
 
-    @FXML
-    private PieChart weeklyOverviewPieChart;
+        @FXML
+        private PieChart weeklyOverviewPieChart;
 
-    @FXML
-    private PieChart monthlyOverviewPieChart;
+        @FXML
+        private PieChart monthlyOverviewPieChart;
+
+        @FXML
+        private Text overviewDailyPieChartText;
+
+        @FXML
+        private Text overviewWeeklyPieChartText;
+
+        @FXML
+        private Text overviewMonthlyPieChartText;
+
 
     public void start(Stage stage) throws Exception {
 
