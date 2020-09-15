8c1ad9c9dc1fc572b0ac0f0557e13b88273f50f3
artinabiri <kevinabiri@gmail.com>
2020-09-15 12:05:44 +0200

    Lagt till tillfällig funktionalitet med knappar på header

--- src/main/java/controllers/headerController.java
+++ src/main/java/controllers/headerController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventTarget;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -14,7 +15,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class headerController {
-
 
 
     //Opens up/going back to first page when pressing on the Logo i.e. works as an escape hatch
@@ -51,16 +51,25 @@ public class headerController {
     private Hyperlink goalsButton;
 
     @FXML
-    private void escapeHatch(ActionEvent event) throws IOException {
-
-        Parent startParent = FXMLLoader.load(getClass().getResource("/org/openjfx/firstPage.fxml"));
+    private void clickHeader(ActionEvent event) throws IOException {
+        Parent startParent;
+        if (homeButton.equals(event.getTarget()) || logo.equals(event.getTarget())) {
+            startParent = FXMLLoader.load(getClass().getResource("/org/openjfx/firstPage.fxml"));
+        } else if (budgetButton.equals(event.getTarget())) {
+            startParent = FXMLLoader.load(getClass().getResource("/org/openjfx/bugetCharPage.fxml"));
+        } else if (entryButton.equals(event.getTarget())) {
+            startParent = FXMLLoader.load(getClass().getResource("/org/openjfx/entryPage.fxml"));
+        } else if (statisticsButton.equals(event.getTarget())) {
+                startParent = FXMLLoader.load(getClass().getResource("/org/openjfx/statisticsOverviewPage.fxml"));
+        } else { throw new IllegalStateException("Unexpected value: " + event.getTarget());
+        }
         Scene startScene = new Scene(startParent);
-
-
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(startScene);
         window.show();
     }
-    }
+        
+
+}
 
 
