a696511f52a6868e9b5171c1bbf28e6ddacce0dd
welzelv <viktoria.welzel@hotmail.de>
2020-09-17 19:31:36 +0200

    more changes

--- src/main/java/controllers/budgetPageController.java
+++ src/main/java/controllers/budgetPageController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
 public class budgetPageController implements ChildPane {
 
     mainController parent;
@@ -11,6 +14,55 @@ public class budgetPageController implements ChildPane {
     public void initPane(mainController parent) {
         this.parent=parent;
         headerAnchorPane.getChildren().setAll(PaneFactory.initHeader());
+
+// TODO -- when writing a value into the textfield the slider showed adjust accordingly
+        //TODO -- values on slider not that specfic round to 10/100
+
+        ChangeListener<Number> changeListener = new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                                Number oldValue, Number newValue) {
+                foodAmountLabel.setText(new Integer((int) foodSlider.getValue()) + " kr");
+                householdAmountLabel.setText(new Integer((int) householdSlider.getValue()) + " kr");
+                shoppingAmountLabel.setText(new Integer((int) shoppingSlider.getValue()) + " kr");
+                transportAmountLabel.setText(new Integer((int) shoppingSlider.getValue()) + " kr");
+                otherAmountLabel.setText(new Integer((int) shoppingSlider.getValue()) + " kr");
+                savingsAmountLabel.setText(new Integer((int) shoppingSlider.getValue()) + " kr");
+
+                //transportTextField.setText(new Integer((int) transportSlider.getValue()) + " kr");
+
+                System.out.println("R,G,B = " + foodSlider.getValue() + "," + householdSlider.getValue() + "," + shoppingSlider.getValue());
+            }
+        };
+
+        foodSlider.valueProperty().addListener(changeListener);
+        householdSlider.valueProperty().addListener(changeListener);
+        shoppingSlider.valueProperty().addListener(changeListener);
+        transportSlider.valueProperty().addListener(changeListener);
+        otherSlider.valueProperty().addListener(changeListener);
+        savingsSlider.valueProperty().addListener(changeListener);
+
+        //foodSlider.setStyle("-fx-base: #F66A80");
+        householdSlider.setStyle("-fx-base: #F66A80");
+        shoppingSlider.setStyle("-fx-base: #F66A80");
+        transportSlider.setStyle("-fx-base: #F66A80");
+        otherSlider.setStyle("-fx-base: #F66A80");
+        savingsSlider.setStyle("-fx-base: #F66A80");
+
+        foodSlider.setStyle("-fx-control-inner-background: #F66A80");
+
+
+        foodAmountLabel.setText("0 kr");
+        householdAmountLabel.setText("0 kr");
+        shoppingAmountLabel.setText("0 kr");
+        transportAmountLabel.setText("0 kr");
+        otherAmountLabel.setText("0 kr");
+        savingsAmountLabel.setText("0 kr");
+
+    }
+
+    private void onSaveButtonPressed(){
+        //budget.setFood(foodTextField.getText);
     }
 
     @FXML
@@ -84,4 +136,8 @@ public class budgetPageController implements ChildPane {
 
     @FXML
     private AnchorPane headerAnchorPane;
+
+    @FXML
+    private TextField enterIncomeTextField;
+
 }
