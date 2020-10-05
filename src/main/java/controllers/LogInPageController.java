aa99622eaf6c90438ceaefec9c39d3fa9c4160a2
artinabiri <kevinabiri@gmail.com>
2020-10-05 15:56:01 +0200

    Update LogInPageController.java

--- src/main/java/controllers/LogInPageController.java
+++ src/main/java/controllers/LogInPageController.java
@@ -59,7 +59,7 @@ public class LogInPageController implements iPane, AccountObserver {
     @FXML
     private void onLoginClicked() {
         parent.showFirstPage();
-/*
+
         LogInHandler logInHandler = new LogInHandler() {
             @Override
             public boolean logIn(TextField usernameField, TextField logInField) {
@@ -77,7 +77,7 @@ public class LogInPageController implements iPane, AccountObserver {
 
         } else {
             userExistanceText.setFill(Color.RED);
-        }*/
+        }
     }
 
 
