400da45124d264d58d59e7732dda3df4c5989897
oscarforsbergg <oscarforsbergg@hotmail.com>
2020-10-08 12:14:02 +0200

    restructure packages in the entire program

---
+++ src/main/java/Controller/AccountControllers/LogInPageController.java
@@ -0,0 +1,91 @@
+package Controller.AccountControllers;
+
+import Controller.MainControllers.MainController;
+import Model.AccountLogic.AccountFacade;
+import Model.Interfaces.AccountObserver;
+import Controller.Interfaces.iPane;
+import Model.AccountLogic.LogInHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class LogInPageController implements iPane, AccountObserver {
+
+    //________________________________________________ VARIABLES _______________________________________________________
+
+    AccountFacade accountFacade = AccountFacade.getInstance();
+
+    //________________________________________________ FXML VARIABLES __________________________________________________
+    @FXML
+    public AnchorPane pos_for_popUp_on_LogInPage;
+    @FXML
+    public AnchorPane back;
+    @FXML
+    public AnchorPane logInContent;
+    private MainController parent;
+    @FXML
+    private PasswordField logInField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private Text message;
+
+    //________________________________________________ Methods _________________________________________________________
+    @Override
+    public void initPane(MainController parent) {
+        this.parent = parent;
+        accountFacade.add(this);
+    }
+
+    @FXML
+    private void signUpButton() {
+
+        back.setVisible(true);
+        pos_for_popUp_on_LogInPage.setVisible(true);
+
+        pos_for_popUp_on_LogInPage.getChildren().clear();
+        pos_for_popUp_on_LogInPage.getChildren().add(parent.getSignUpPopUp());
+        back.toFront();
+        back.setStyle("-fx-background-color: #000000");
+        back.setOpacity(0.5);
+        pos_for_popUp_on_LogInPage.toFront();
+    }
+
+
+    @FXML
+    private void onLoginClicked() {
+        LogInHandler logInHandler = new LogInHandler() {
+            @Override
+            public boolean logIn(TextField usernameField, TextField logInField) {
+                return super.logIn(usernameField, logInField);
+            }
+        };
+
+        boolean caseNumber = logInHandler.logIn(usernameField, logInField);
+
+        if (caseNumber) {
+            message.setFill(Color.WHITE);
+            usernameField.clear();
+            logInField.clear();
+            parent.showFirstPage();
+
+        } else {
+            message.setFill(Color.RED);
+        }
+    }
+
+    @FXML
+    private void hidePopUp() {
+        back.setVisible(false);
+        pos_for_popUp_on_LogInPage.setVisible(false);
+        logInContent.toFront();
+    }
+
+    @Override
+    public void update() {
+        hidePopUp();
+    }
+}
---
+++ src/main/java/Controller/MainControllers/HeaderController.java
@@ -0,0 +1,65 @@
+package Controller.MainControllers;
+
+import Model.AccountLogic.AccountFacade;
+import Model.Interfaces.AccountObserver;
+import Controller.Interfaces.iPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+public class HeaderController implements iPane, AccountObserver {
+
+    //________________________________________________ VARIABLES _______________________________________________________
+
+    MainController parent;
+    AccountFacade accountFacade = AccountFacade.getInstance();
+
+    //________________________________________________ FXML ____________________________________________________________
+
+    @FXML
+    private Hyperlink accountPageButton;
+    @FXML
+    private Hyperlink logo;
+    @FXML
+    private Hyperlink homeButton;
+    @FXML
+    private Hyperlink entryButton;
+    @FXML
+    private Hyperlink statisticsButton;
+    @FXML
+    private Hyperlink budgetButton;
+    @FXML
+    private Hyperlink goalButton;
+
+    //________________________________________________ Methods _________________________________________________________
+
+    @FXML
+    private void clickHeader(ActionEvent event) {
+        if (homeButton.equals(event.getTarget()) || logo.equals(event.getTarget())) {
+            parent.showFirstPage();
+        } else if (budgetButton.equals(event.getTarget())) {
+            parent.showBudgetCharPage();
+        } else if (entryButton.equals(event.getTarget())) {
+            parent.showEntryPage();
+        } else if (statisticsButton.equals(event.getTarget())) {
+            parent.showStatisticsOverviewPage();
+        } else if (accountPageButton.equals((event.getTarget()))) {
+            parent.showAccountPage();
+        } else if (goalButton.equals(event.getTarget())) {
+            parent.showGoalsPage();
+        }
+    }
+
+    @Override
+    public void initPane(MainController parent) {
+        this.parent = parent;
+        accountFacade.add(this);
+    }
+
+    @Override
+    public void update() {
+        accountPageButton.setText(accountFacade.getAccountName());
+    }
+}
+
+
