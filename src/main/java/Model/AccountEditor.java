cc5d5ab32ae3ae982bf6142c850858a1a4599d14
Oscaarr <43784002+Oscarforsbergg@users.noreply.github.com>
2020-10-08 12:26:28 +0200

    Revert "restructure packages in the entire program"

---
+++ src/main/java/Model/AccountEditor.java
@@ -0,0 +1,72 @@
+package Model;
+
+import javafx.scene.control.TextField;
+
+public class AccountEditor {
+    //________________________________________________ Variables _______________________________________________________
+    private static AccountEditor accountEditor;
+
+    Account account = Account.getInstance();
+
+    AccountValidityChecker accountValidityChecker = AccountValidityChecker.getInstance();
+
+    //________________________________________________ Methods _________________________________________________________
+
+    public static AccountEditor getInstance() {
+        if (accountEditor == null) {
+            accountEditor = new AccountEditor();
+        }
+        return accountEditor;
+
+    }
+
+    boolean setAccountName(TextField textField) {
+        if (accountValidityChecker.isValidName(textField.getText())) {
+            account.setName(textField.getText());
+            return true;
+        } else textField.setStyle("-fx-text-box-border: #B22222;");
+        return false;
+    }
+
+    String getAccountName() {
+        return account.getName();
+    }
+
+    boolean setAccountEmail(TextField textField) {
+        if (accountValidityChecker.isValidEmail(textField.getText())) {
+            account.setEmailAddress(textField.getText());
+            return true;
+        } else textField.setStyle("-fx-text-box-border: #B22222;");
+        return false;
+
+    }
+
+    boolean setAccountUsername(TextField textField) {
+        if (textField.getText() != null) {
+            account.setUsername(textField.getText());
+            return true;
+        } else textField.setStyle("-fx-text-box-border: #B22222;");
+        return false;
+    }
+
+    boolean setAccountPassword(TextField textField) {
+        if (accountValidityChecker.checkPasswordLength(textField.getText())) {
+            account.setPassword(textField.getText());
+            return true;
+        } else textField.setStyle("-fx-text-box-border: #B22222;");
+        return false;
+    }
+
+    boolean isAccountPageFieldsCorrect(TextField name, TextField email, TextField username) {
+        return (accountValidityChecker.isValidName(name.toString()) && accountValidityChecker.isValidEmail(email.toString()) && !username.getText().equals(""));
+
+    }
+
+    public String getAccountUsername() {
+        return account.getUsername();
+    }
+
+    public String getAccountEmail() {
+        return account.getEmailAddress();
+    }
+}
---
+++ src/main/java/Model/AccountFacade.java
@@ -0,0 +1,80 @@
+package Model;
+
+import Model.Interfaces.AccountObserver;
+import Model.Interfaces.AccountSubject;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountFacade implements AccountSubject {
+    private static AccountFacade accountFacade;
+
+    private final List<AccountObserver> UserObservers = new ArrayList<>();
+
+    AccountExistenceManager accountExistenceManager = AccountExistenceManager.getInstance();
+    AccountEditor accountEditor = AccountEditor.getInstance();
+
+
+    private AccountFacade() {
+        this.accountExistenceManager = accountExistenceManager;
+        this.accountEditor = accountEditor;
+    }
+
+    public static AccountFacade getInstance() {
+        if (accountFacade == null) {
+            accountFacade = new AccountFacade();
+        }
+        return accountFacade;
+    }
+
+    public String getAccountName() {
+        return accountEditor.getAccountName();
+
+    }
+
+    public String getAccountUsername() {
+        return accountEditor.getAccountUsername();
+
+    }
+
+    public String getAccountEmail() {
+        return accountEditor.getAccountEmail();
+
+    }
+
+    //Methods
+    public Account createAccount(TextField signUpName, TextField signUpUsername,
+                                 TextField signUpPassword, TextField signUpConfirmPassword, TextField signUpEmail) {
+        return accountExistenceManager.createAccount(signUpName, signUpUsername,
+                signUpPassword, signUpConfirmPassword, signUpEmail);
+    }
+
+    public void deleteAccount() {
+        accountExistenceManager.deleteAccount();
+    }
+
+    @Override
+    public void add(AccountObserver o) {
+        UserObservers.add(o);
+
+    }
+
+    @Override
+    public void notifyListeners() {
+        for (AccountObserver observer : UserObservers) {
+            observer.update();
+        }
+
+    }
+
+    public boolean isAccountPageFieldsCorrect(TextField name, TextField email, TextField username) {
+        return accountEditor.isAccountPageFieldsCorrect(name, email, username);
+    }
+
+    public boolean checkPasswordMatch(String inputPassword) {
+        return accountExistenceManager.checkPasswordMatch(inputPassword);
+
+    }
+
+}
