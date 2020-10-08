a501b021da7db5bea5aadd04b3d1fd555ff144e0
Lenia <lenia_malki@hotmail.com>
2020-10-08 20:08:26 +0200

    JavaDoc

--- src/main/java/Controller/AccountControllers/LogInPageController.java
+++ src/main/java/Controller/AccountControllers/LogInPageController.java
@@ -1,15 +1,20 @@
 package Controller.AccountControllers;
 
+import Controller.Interfaces.iPane;
 import Controller.MainControllers.MainController;
 import Model.AccountLogic.AccountFacade;
-import Model.Interfaces.AccountObserver;
-import Controller.Interfaces.iPane;
 import Model.AccountLogic.LogInHandler;
+import Model.Interfaces.AccountObserver;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+
+/**
+ * Controller for login on login page
+ * @author Lenia
+ */
 
 public class LogInPageController implements iPane, AccountObserver {
 
@@ -17,7 +22,6 @@ public class LogInPageController implements iPane, AccountObserver {
 
     private final AccountFacade accountFacade = AccountFacade.getInstance();
 
-    //________________________________________________ FXML VARIABLES __________________________________________________
     @FXML
     public AnchorPane pos_for_popUp_on_LogInPage;
     @FXML
@@ -33,12 +37,20 @@ public class LogInPageController implements iPane, AccountObserver {
     private Text message;
 
     //________________________________________________ Methods _________________________________________________________
+
+    /**
+     * initialized this pane
+     */
     @Override
     public void initPane(MainController parent) {
         this.parent = parent;
         accountFacade.add(this);
     }
 
+    /**
+     * Handles the mouse action of opening up the signup pop up. The pop up is disabled if an account already exists
+     * by checking if there instance of account has a name assigned to it.
+     */
     @FXML
     private void signUpButton() {
 
@@ -52,11 +64,14 @@ public class LogInPageController implements iPane, AccountObserver {
             back.setStyle("-fx-background-color: #000000");
             back.setOpacity(0.5);
             pos_for_popUp_on_LogInPage.toFront();
-        }
-        else message.setText("An account already exists! ");
+        } else message.setText("An account already exists! ");
 
     }
 
+    /**
+     * Handles the mouse action of logging in to the first page. The logic of checking log in fields is partly managed
+     * by the account facade and logInHandler.
+     */
     @FXML
     private void onLoginClicked() {
         LogInHandler logInHandler = new LogInHandler() {
@@ -68,8 +83,7 @@ public class LogInPageController implements iPane, AccountObserver {
 
         if (accountFacade.getAccountName() == null) {
             message.setText("No user registered!");
-        }
-        else if (logInHandler.logIn(usernameField, logInField)) {
+        } else if (logInHandler.logIn(usernameField, logInField)) {
             message.setText("");
             usernameField.setStyle("-fx-text-box-border: #ffffff");
             logInField.setStyle("-fx-text-box-border: #ffffff");
@@ -79,10 +93,11 @@ public class LogInPageController implements iPane, AccountObserver {
         }
 
 
-
-
     }
 
+    /**
+     * Graphic method to hide pop ups
+     */
     @FXML
     private void hidePopUp() {
         back.setVisible(false);
@@ -90,6 +105,9 @@ public class LogInPageController implements iPane, AccountObserver {
         logInContent.toFront();
     }
 
+    /**
+     * Whenever a user gets registered, the signup page is hidden
+     */
     @Override
     public void update() {
         hidePopUp();
--- src/main/java/Model/AccountLogic/AccountFacade.java
+++ src/main/java/Model/AccountLogic/AccountFacade.java
@@ -7,22 +7,27 @@ import javafx.scene.control.TextField;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is a facade which delegates work to AccountExistenceManager and AccountEditor in order to create, delete
+ * and change the variables of an account.
+ *
+ * @author Lenia
+ */
 public class AccountFacade implements AccountSubject {
     private static AccountFacade accountFacade;
-
-    private final List<AccountObserver> UserObservers = new ArrayList<>();
-
     AccountExistenceManager accountExistenceManager = AccountExistenceManager.getInstance();
     AccountEditor accountEditor = AccountEditor.getInstance();
+    private final List<AccountObserver> UserObservers = new ArrayList<>();
 
-    public boolean checkPasswordValidity (TextField password) {
-        return accountEditor.setAccountPassword(password);
-    }
-
-
+    /**
+     * Private constructor
+     */
     private AccountFacade() {
     }
 
+    /**
+     * Method for obtaining the account facade instance.
+     */
     public static AccountFacade getInstance() {
         if (accountFacade == null) {
             accountFacade = new AccountFacade();
@@ -30,51 +35,106 @@ public class AccountFacade implements AccountSubject {
         return accountFacade;
     }
 
+    /**
+     * Getter
+     */
     public String getAccountName() {
         return accountEditor.getAccountName();
-
     }
 
+    /**
+     * Sets account name through delegation to accountEditor
+     *
+     * @param name is passed in by the controller in use of the method
+     */
+    public void setAccountName(TextField name) {
+        accountEditor.setAccountName(name);
+    }
+
+    /**
+     * Gets account username through delegation to accountEditor
+     *
+     * @return returns account username
+     */
     public String getAccountUsername() {
         return accountEditor.getAccountUsername();
 
     }
 
+    /**
+     * Sets account usernamename through delegation to accountEditor
+     *
+     * @param username is passed in by the controller in use of the method
+     */
+    public void setAccountUsername(TextField username) {
+        accountEditor.setAccountUsername(username);
+    }
+
+    /**
+     * Gets account email through delegation to accountEditor
+     *
+     * @return returns account email
+     */
     public String getAccountEmail() {
         return accountEditor.getAccountEmail();
 
     }
 
-    public void setAccountName (TextField name) {
-        accountEditor.setAccountName(name);
-    }
-    public void setAccountUsername (TextField username) {
-        accountEditor.setAccountUsername(username);
-    }
-    public boolean setAccountPassword (TextField  password) {
-        return accountEditor.setAccountPassword(password);
-    }
-
-    public void setAccountEmail (TextField email) {
+    /**
+     * Sets account email through delegation to accountEditor
+     *
+     * @param email is passed in by the controller in use of the method
+     */
+    public void setAccountEmail(TextField email) {
         accountEditor.setAccountEmail(email);
     }
 
+    /**
+     * Sets account password through delegation to accountEditor
+     *
+     * @param password is passed in by the controller in use of the method
+     * @return returns a boolean value if the username is valid
+     */
+    public boolean setAccountPassword(TextField password) {
+        return accountEditor.setAccountPassword(password);
+    }
+
+    /**
+     * Delegates the creation of an account to the account existence manager
+     *
+     * @param signUpName            is passed in by the getText() method of corresponding textField in controller signUp
+     * @param signUpUsername        is passed in by the getText() method of corresponding textField in controller signUp
+     * @param signUpPassword        is passed in by the getText() method of corresponding textField in controller signUp
+     * @param signUpConfirmPassword is passed in by the getText() method of corresponding textField in controller
+     *                              signUp
+     * @param signUpEmail           is passed in by the getText() method of corresponding textField in controller signUp
+     * @return returns a created account
+     */
     public Account createAccount(TextField signUpName, TextField signUpUsername,
                                  TextField signUpPassword, TextField signUpConfirmPassword, TextField signUpEmail) {
         return accountExistenceManager.createAccount(signUpName, signUpUsername,
-                signUpPassword, signUpConfirmPassword, signUpEmail);
+                                                     signUpPassword, signUpConfirmPassword, signUpEmail);
     }
 
+
+    /**
+     * Delegates the deletion of an account to the account existence manager
+     */
     public void deleteAccount() {
         accountExistenceManager.deleteAccount();
     }
 
+    /**
+     * Adds observers to its litst of UserObservers
+     */
     @Override
     public void add(AccountObserver o) {
         UserObservers.add(o);
-
     }
 
+    /**
+     * Notifies all of its observers in its oserverlist to update
+     */
     @Override
     public void notifyListeners() {
         for (AccountObserver observer : UserObservers) {
@@ -83,14 +143,16 @@ public class AccountFacade implements AccountSubject {
 
     }
 
-    public String getAccountPassword () {
-       return accountEditor.getAccountPassword();
-    }
-
+    /**
+     * Delegates the check for the changing of accont page's fields to accountEditor
+     */
     public boolean isAccountPageFieldsCorrect(TextField username, TextField name, TextField email) {
         return accountEditor.areAccountFieldsCorrect(username, name, email);
     }
 
+    /**
+     * Delegates the check for password match to accountExistanceManager
+     */
     public boolean checkPasswordMatch(String inputPassword) {
         return accountExistenceManager.checkPasswordMatch(inputPassword);
 
