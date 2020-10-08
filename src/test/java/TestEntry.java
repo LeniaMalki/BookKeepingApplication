bddf68650bab7121bf7e0d05eac94e860ff46399
artinabiri <kevinabiri@gmail.com>
2020-10-08 11:11:27 +0200

    TEST IMPLEMENTERAT FUCK YEEESS

---
+++ src/test/java/TestEntry.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestEntry {
+
+    @Test
+    public void testSomething(){
+        int i=4;
+        assertEquals(i,4);
+    }
+
+}
