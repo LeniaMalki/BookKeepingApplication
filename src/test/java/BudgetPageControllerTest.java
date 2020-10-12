03b5fdbcb75276a7d9b1779969bc99cd474eb8ff
welzelv <viktoria.welzel@hotmail.de>
2020-10-12 14:00:11 +0200

    test

---
+++ src/test/java/BudgetPageControllerTest.java
@@ -0,0 +1,25 @@
+import Model.BudgetLogic.Budget;
+import javafx.scene.control.Slider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BudgetPageControllerTest {
+
+Budget budget;
+Slider slider;
+
+    @BeforeEach
+    public void setUp() {
+        budget = new Budget(200,200,200,200, 200, 200, "1200");
+    }
+
+    @Test
+    public void setMaxOnSliderTest(){
+        slider.setMax(1200);
+    }
+
+    @Test
+    public void updatingMoneyLeftTest (){
+
+    }
+}
