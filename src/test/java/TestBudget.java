0a27775dcb38ff5016b43ecb551a694b8e5bfb2a
welzelv <viktoria.welzel@hotmail.de>
2020-10-12 13:09:25 +0200

    budget test

--- src/test/java/TestBudget.java
+++ src/test/java/TestBudget.java
@@ -1,30 +1,46 @@
 import Model.BudgetLogic.Budget;
+import Model.EntryLogic.Entry;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestBudget {
 
-private Budget budget;
+        Budget budget;
 
-    @Test
-    public void test_Budget_default_constructor(){
-        budget.setFoodCost(200);
-        assertEquals(200,budget.getFoodCost());
+        @BeforeEach
+        public void createNewEntry(){
+            budget = new Budget(100,100,100,100, 100, 100, "100");
+        }
+        @Test
+        public void testCreationOfFoodCost(){
+            Assertions.assertEquals(budget.getFoodCost(), 100);
+        }
+        @Test
+        public void testCreationOfHouseholdCost(){
+            Assertions.assertEquals(budget.getHouseholdCost(), 100);
+        }
+        @Test
+        public void testCreationOfShoppingCost(){
+            Assertions.assertEquals(budget.getShoppingCost(), 100);
+        }
+        @Test
+        public void testCreationOfTransportCost(){
+            Assertions.assertEquals(budget.getTransportCost(), 100);
+        }
+        @Test
+        public void testCreationOfSavingsCost(){
+            Assertions.assertEquals(budget.getSavingsCost(), 100);
+        }
+        @Test
+        public void testCreationOfOtherCost(){
+            Assertions.assertEquals(budget.getOtherCost(), 100);
+        }
+        @Test
+        public void testCreationOfIncome(){
+            Assertions.assertEquals(budget.getIncome(), "100");
+        }
 
-      /*  budget.setHouseholdCost(200);
-        assertEquals(200,budget.getHouseholdCost());
-
-        budget.setShoppingCost(200);
-        assertEquals(200,budget.getShoppingCost());
-
-        budget.setTransportCost(200);
-        assertEquals(200,budget.getTransportCost());
-
-        budget.setSavingsCost(200);
-        assertEquals(200,budget.getSavingsCost());
-
-        budget.setOtherCost(200);
-        assertEquals(200,budget.getOtherCost());*/
-    }
 }
